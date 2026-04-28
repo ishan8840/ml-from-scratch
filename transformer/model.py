ee424d0f22cb1459c07e8335354dcd64f7c87b25
Ishan Shah <ishah8840@gmail.com>
2026-04-27 20:17:44 -0400

    added token emdeddings

--- transformer/model.py
+++ transformer/model.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 
 
 # https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
@@ -7,6 +8,7 @@ with open('input.txt', 'r') as f:
 
 
 chars = ''.join(sorted(set(text)))
+vocab_size = len(chars)
 
 # encoding and decoding
 stoi = { ch:i for i, ch in enumerate(chars)}
@@ -29,7 +31,20 @@ def get_batch(batch_size, chunk_size):
     return x, y
 
 
-x, y = get_batch(4, 8)
 
-print(x[1])
-print(y[1])
+class GPT(nn.Module):
+
+    def __init__(self, vocab_size, d_model):
+        super().__init__()
+        self.token_embedding = nn.Embedding(vocab_size, d_model) # (B, T, C)
+    
+    def forward(self, idx):
+        x = self.token_embedding(idx)
+
+        return x
+    
+model = GPT(vocab_size, d_model=64)
+xb, yb = get_batch(4, 8)
+out = model(xb)
+
+print(out.shape)
